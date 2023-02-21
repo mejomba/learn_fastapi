ae08b2e82fe3e9bd682b585a0716426af72aa037
mejomba <moojaminzadeh@gmail.com>
2023-02-22 00:12:56 +0330

    02 orm

--- 02_using_database/02_orm/02_use_orm.py
+++ 02_using_database/02_orm/02_use_orm.py
@@ -15,20 +15,11 @@ models.base.metadata.create_all(bind=engin)
 app = FastAPI()
 
 
-
-
-
 class Post(BaseModel):
     title: str
     content: str
-    publish: bool = True  # this is optional field
-    rating: Optional[int] = None  # this is fully optional
-
-
-@app.get('/sqlalcheme')
-def test(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {'data': posts}
+    published: bool = True  # this is optional field
+    # rating: Optional[int] = None  # this is fully optional
 
 
 while True:
@@ -48,6 +39,7 @@ my_posts = [{'title': 'post 1 title', 'content': "post 1 content", 'id': 1},
             {'title': 'post 3 title', 'content': "post 3 content", 'id': 3},
             ]
 
+
 def get_index(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
@@ -55,58 +47,52 @@ def get_index(id):
 
 
 @app.get('/posts')
-def get_post():
-    cur.execute("""SELECT * FROM post""")
-    posts = cur.fetchall()
+def get_post(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
     return {'data': posts}
 
 
 @app.post('/createpost', status_code=status.HTTP_201_CREATED)  # change default status code
-def create_post(payload: Post):
-    query = """INSERT INTO post (title, content, published) VALUES(%s, %s, %s) RETURNING *"""
-    data = payload.title, payload.content, payload.publish
-    cur.execute(query, data)
-    new_post = cur.fetchone()
-    conn.commit()
+def create_post(payload: Post, db: Session = Depends(get_db)):
+    # new_post = models.Post(title=payload.title, content=payload.content, published=payload.published)
+    new_post = models.Post(**payload.dict())
+    print(new_post)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)  # like RETURNING in sql statement
     return new_post
 
 
 @app.get('/posts/{id}')
-def get_post(id: int, response: Response):
-    query = """SELECT * FROM post WHERE post_id=(%s)"""
-    data = id,
-    cur.execute(query, data)
-    post = cur.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    print(post)
     if post:
         return {'data': post}
     else:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'message': 'not found'}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='not found')
 
 
 @app.delete('/delete/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    query = """DELETE FROM post WHERE post_id=(%s) RETURNING *"""
-    data = id,
-    cur.execute(query, data)
-    deleted_post = cur.fetchone()
-    conn.commit()
-    if deleted_post:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    print(post)
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
+    else:
+        post_query.delete(synchronize_session=False)
+        db.commit()
 
 
 @app.put('/posts/{id}', status_code=status.HTTP_206_PARTIAL_CONTENT)
-def update_post(id: int, post: Post):
-    query = """UPDATE post SET title=%s, content=%s, published=%s WHERE post_id=%s RETURNING *"""
-    data = post.title, post.content, post.publish, id
-    cur.execute(query, data)
-    updated_post = cur.fetchone()
-    conn.commit()
+def update_post(id: int, payload: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if updated_post:
-        return {'data': updated_post}
+    if post is not None:
+        post_query.update(payload.dict(), synchronize_session=False)
+        db.commit()
+        return {'data': post_query.first()}
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
